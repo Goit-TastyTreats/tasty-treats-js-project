1c05b64af2a9a6738910fcad90abd5f91dc19a74
mertkrglu <mertkrgluphoto@gmail.com>
2026-04-24 12:52:11 +0300

    denemeler devam

--- src/js/recipe-cards.js
+++ src/js/recipe-cards.js
@@ -8,21 +8,23 @@ export async function renderRecipeCards(filters = {}) {
 
     const markup = results
       .map(
-        ({ _id, title, preview, description, rating, tags }) => `
-      <li class="recipe-card" data-id="${_id}">
-        <div class="card-img-wrapper">
-          <img src="${preview}" alt="${title}" class="recipe-card-img">
-        </div>
-        <div class="card-content">
-          <h3 class="card-title">${title}</h3>
-          <p class="card-description">${description}</p>
-          <div class="card-footer">
-            <span class="card-rating">${rating.toFixed(1)}</span>
-            <button type="button" class="see-recipe-btn">See recipe</button>
-          </div>
-        </div>
-      </li>
-    `
+        ({ _id, title, preview, description, rating }) => `
+  <li class="recipe-card" data-id="${_id}">
+    <button type="button" class="favorite-btn">
+       <svg class="heart-icon" width="22" height="22">
+          </svg>
+    </button>
+    <img src="${preview}" alt="${title}" class="recipe-card-img">
+    <div class="card-content">
+      <h3 class="card-title">${title}</h3>
+      <p class="card-description">${description}</p>
+      <div class="card-footer">
+        <span class="card-rating">${rating.toFixed(1)}</span>
+        <button type="button" class="see-recipe-btn">See recipe</button>
+      </div>
+    </div>
+  </li>
+`
       )
       .join('');
 
