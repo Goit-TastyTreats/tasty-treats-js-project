788637c8a707f81906fe68802a97e46ab123f34a
aburakozaslan <abdullahburak564@gmail.com>
2026-04-24 19:58:05 +0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // src/main.js
+import './js/hero.js';
 import './js/all-category.js';
 import './js/popular-recipes.js'; 
 import './js/recipe-cards.js';
 import './js/popular-recipes.js';
 import './js/filters.js';
-import './js/hero.js';
