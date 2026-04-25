05f6d28588d0cbde9a6ace5260f812d429860b45
gulistanuzun <uzun.gulistan1@gmail.com>
2026-04-25 22:19:21 +0300

    hero completed

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 // src/main.js
 import './js/hero.js';
 import './js/all-category.js';
-import './js/popular-recipes.js'; 
-import './js/recipe-cards.js';
 import './js/popular-recipes.js';
+import './js/recipe-cards.js';
 import './js/filters.js';
 import './js/favorites.js';
