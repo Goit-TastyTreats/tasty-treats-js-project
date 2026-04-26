d0ab7aca19fb3aed9953a564986682fbeeab9f2c
Doga Ceren Erkek <dogacerenerkek@gmail.com>
2026-04-26 13:33:51 +0200

    logo boyut düzeltildi, light dark moddaki renkler  düzeltildi,burger menu üzerine çalışıldı, js eklemeleri yapıldı, dark mode için çalışıldı

--- src/js/header.js
+++ src/js/header.js
@@ -1,4 +1,4 @@
-
+const savedTheme = localStorage.getItem('theme');
 
 const hamburgerBtn = document.getElementById('hamburgerBtn');
 
@@ -6,63 +6,70 @@ const mobileMenu = document.getElementById('mobileMenu');
 
 const closeMenuBtn = document.getElementById('closeMenuBtn');
 
-
 // burger açma
 
 hamburgerBtn.addEventListener('click', () => {
-
   mobileMenu.classList.add('active');
-
 });
-
 
 // burger kapama
 
 closeMenuBtn.addEventListener('click', () => {
-
   mobileMenu.classList.remove('active');
-
 });
 
-
 // ACTIVE PAGE LINK
-
 
 const currentPage = window.location.pathname;
 
 const desktopLinks = document.querySelectorAll('.nav-link');
 
 desktopLinks.forEach(link => {
-
   const linkPath = new URL(link.href).pathname;
 
   if (currentPage === linkPath) {
-
     link.classList.add('active-link');
-
   }
-
 });
-// =========================
-// THEME TOGGLE
-// =========================
+
+// TOGGLE tema
 
 const themeToggle = document.getElementById('themeToggle');
 
 const desktopThemeToggle = document.getElementById('desktopThemeToggle');
 
+if (savedTheme === 'dark') {
+  document.body.classList.add('dark-theme');
+}
 
-// mobile toggle
 themeToggle.addEventListener('click', () => {
-
   document.body.classList.toggle('dark-theme');
 
+  if (document.body.classList.contains('dark-theme')) {
+    localStorage.setItem('theme', 'dark');
+  } else {
+    localStorage.setItem('theme', 'light');
+  }
+});
+desktopThemeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme');
+
+  if (document.body.classList.contains('dark-theme')) {
+    localStorage.setItem('theme', 'dark');
+  } else {
+    localStorage.setItem('theme', 'light');
+  }
 });
 
+// ORDER MODAL penceresi
 
-// desktop toggle
-desktopThemeToggle.addEventListener('click', () => {
+const openOrderModalBtn = document.getElementById('openOrderModal');
 
-  document.body.classList.toggle('dark-theme');
+const orderModal = document.getElementById('orderModal');
 
+openOrderModalBtn.addEventListener('click', event => {
+  event.preventDefault();
+
+  // modal aç
+  orderModal.classList.add('is-open');
 });
