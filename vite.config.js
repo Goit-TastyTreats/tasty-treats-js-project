dfb61442ab2dbffe34c1d6c74aefa8b2ba816bc5
aburakozaslan <abdullahburak564@gmail.com>
2026-05-01 01:20:39 +0300

    dosya

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig(({ command }) => {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
+     base: '/tasty-treats-js-project/',
     root: './', 
     
     build: {
